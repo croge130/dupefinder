be2459be8686f70571a2d9b868f48782de561e7a
Christian R <chris@taffle.me>
2019-01-06 09:21:31 -0500

    Added an incomplete hash_folder function to be used as a pthread start.

--- main.c
+++ main.c
@@ -4,14 +4,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
-
 #include <openssl/md5.h>
 #include <fcntl.h>
+#include <pthread.h>
 
+//#define DEFAULT_PATH "/mnt/c/Users/Chris/Documents/test/dupe_finder"
 
-#define DEFAULT_PATH "/mnt/c/Users/Chris/Documents/test/dupe_finder"
-#define MD5_DIGEST_LEN 64
 
 int file_md5(unsigned char* digest, char md5_buf[64], size_t buffsize, const char* path);
 
@@ -20,6 +18,7 @@ const char* ignore_subdirs[] = {".", "..", "data"}; // files to ignore
 
 int main(int argc, char* argv[])
 {
+    pthread_t *threads[10];
     char* scan_path;
     char  pathbuf[1024];
 
@@ -44,6 +43,7 @@ int main(int argc, char* argv[])
     }
 
     while ((dirfile = readdir(maindir)) != NULL) {
+        // TODO: Rewrite to avoid using chdir(2)
         chdir(scan_path); // would it be faster to use fchdir(2) instead? probably, but not noticeably.
         if (dirfile->d_type == DT_DIR) { // not a dir?
             if (strcmp(dirfile->d_name, ".") == 0 || strcpy(dirfile->d_name, "..") == 0)
@@ -76,6 +76,35 @@ int main(int argc, char* argv[])
     closedir(maindir);
     return 0;
 }
+// TODO: Use ptreads to md5 hash the files in separate folders (maybe like 5-10 at a time?)
+// TODO: Use some struct that points to a structure for this thread to store the md5 hashes.
+// TODO: Use hashmaps (crc32?) to store md5 hash with crc32
+void* hash_folder (void* param) // uses void* because pthread.
+{
+    struct dirent* dirfile = (struct dirent*)param;
+    struct dirent* subfile;
+    DIR* dir;
+    int fd;
+
+    if ((fd = open(dirfile->d_name, O_RDONLY) == -1)) { // need dirfd for openat(2);
+        perror("Failed to open directory"); // could use snprintf to format to buffer but whatever.
+        // TODO: determine whether perror adds \n
+        printf("\t%s", dirfile->d_name);
+        pthread_exit(NULL);
+    } else {
+        if ((dir = fdopendir(fd)) == NULL) {
+            perror("failed to fopendir(): ");
+            printf("\t%s", dirfile->d_name);
+            pthread_exit(NULL);
+        }
+
+    }
+
+    while (subfile = readdir())
+
+
+
+}
 
 
 int file_md5 (unsigned char *digest, char *md5_buffer, size_t buffsize, const char* path) // md5 is good enough
@@ -93,7 +122,6 @@ int file_md5 (unsigned char *digest, char *md5_buffer, size_t buffsize, const ch
     while((bytes_read = read(fd, md5_buffer, buffsize)) > 0)
         MD5_Update(md5, md5_buffer, (size_t)bytes_read);
     MD5_Final(digest,md5);
-
 
     close(fd);
     return 0;
